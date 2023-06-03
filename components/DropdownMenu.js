4fe4bbd39e0b65a7098d651bdfd183f616b80c0a
getulio.souza <getulio.souza@evolutix.com.br>
2023-06-03 16:47:55 -0300

    dropdown-first-version

--- components/DropdownMenu.js
+++ components/DropdownMenu.js
@@ -2,3 +2,40 @@ import React from 'react'
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 
 
+export default function DropdownMenu() {
+  return (
+    <View style={styles.dropdownContainer}>
+
+      <View>
+        <View>logo</View>
+        <View>X</View>
+      </View>
+
+      <View style={styles.categoriesContainer}>
+        <View style={styles.category}>
+          <View>Icone</View>
+          <Text>Monitoramento</Text>
+        </View>
+
+        <View style={styles.category}>
+          <View>Icone</View>
+          <Text>Otimização de rotas</Text>
+        </View>
+
+        <View style={styles.category}>
+          <View>Icone</View>
+          <Text>Simulações de consumo</Text>
+        </View>
+
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  dropdownContainer: {
+    height: '100%',
+    width: '50%',
+    backgroundColor: '#8EDB87'
+  }
+})
